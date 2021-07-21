074295f4dc272d9fd8b475b89b5910f36c511a97
TranLam <tranngoclamjapan@gmail.com>
2021-07-21 20:57:22 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ const port = 3002;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars({
@@ -24,6 +28,15 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('');
 })
 
 app.listen(port, () => {
