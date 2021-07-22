3199d0775c5eb7e7d6d4d59e7cb34429f34c6c9e
TranLam <tranngoclamjapan@gmail.com>
2021-07-22 19:46:14 +0700

    Create new course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-  name: { type: String, maxlength: 255 },
-  description: { type: String, maxlength: 600 },
-  image: { type: String, maxlength: 255 },
-  createAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+  {
+    name: { type: String, require: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, require: true },
+    level: { type: String },
+    slug: { type: String, slug: "name", unique: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Course", Course);
